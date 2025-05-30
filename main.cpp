7b20d8ee83c5b007f2d384d3df95c976cbed3b62
Juste Vaitkeviciute <juste@Justes-MacBook-Air.local>
2025-05-30 22:10:08 +0300

    Sukurta konvertavimo funkcija

--- main.cpp
+++ main.cpp
@@ -10,6 +10,16 @@
 
 using namespace std;
 
+string convert(const string& word) {
+    string result;
+    for (char ch : word) {
+        if (isalpha(ch)) {
+            result += tolower(ch);
+        }
+    }
+    return result;
+}
+
 int main() {
     ifstream input("input.txt");
     ofstream word_output("output.txt");
@@ -23,6 +33,28 @@ int main() {
     map<string, int> word_count;
     map<string, set<int>> word_lines;
 
+    string line;
+    int line_number = 0;
 
+    while (getline(input, line)) {
+        ++line_number;
+
+        istringstream iss(line);
+        string word;
+        while (iss >> word) {
+            string converted = convert(word);
+            if (!converted.empty()) {
+                word_count[converted]++;
+                word_lines[converted].insert(line_number);
+            }
+        }
+    }
+
+    for (const auto& [word, count] : word_count) {
+        if (count > 1) {
+            word_output << word << ": " << count << "\n";
+        }
+    }
+    
     return 0;
 }
